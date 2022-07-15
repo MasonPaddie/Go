14545ed917a73981e03b7115d02551ade3eb573a
MasonPaddie <masonpaddie29@yahoo.com>
2022-07-14 20:02:10 -0500

    Home screen

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const startGame = () => {
+    document.getElementsByClassName('welcomeButton')[0].style.display = 'none'
+    document.getElementsByClassName('startButton')[0].style.display = 'none'
+    document.getElementsByClassName('howToButton')[0].style.display = 'none'
+   
+    document.body.style.backgroundImage = 'url(images/goBoard.png)'
+}
+
+const howToLink = () => {
+    
+}
